ad25070602cc98c14c9e696ca0ae2d59aa5adb8d
Stanislav Yashenkov <vampire@realtymag.ru>
2015-04-25 21:15:45 +0300

    expressions AST

--- ast.js
+++ ast.js
@@ -72,6 +72,82 @@ function Attribute(name, value) {
 
 /**
  * @constructor
+ * @extends Operand
+ * @property {string} name
+ * @param {string} name
+ */
+function Identifier(name) {
+    this.name = typeof name !== 'undefined' ? String(name) : null;
+}
+
+/**
+ * @constructor
+ * @extends Operand
+ * @property {*} value
+ * @param {*} value
+ */
+function Value(value) {
+    this.value = value;
+}
+
+/**
+ * @class Operand
+ * @abstract
+ */
+/**
+ * @class Operator
+ * @extends Operand
+ * @abstract
+ * @property {number} type
+ */
+
+/**
+ * @constructor
+ * @extends Operator
+ * @property {Operand} a
+ * @param {number} type
+ * @param {Operand} a
+ */
+function UnaryOperator(type, a) {
+    this.type = type;
+    this.a = a;
+}
+
+/**
+ * @constructor
+ * @extends Operator
+ * @property {Operand} a
+ * @property {Operand} b
+ * @param {number} type
+ * @param {Operand} a
+ * @param {Operand} b
+ */
+function BinaryOperator(type, a, b) {
+    this.type = type;
+    this.a = a;
+    this.b = b;
+}
+
+/**
+ * @constructor
+ * @extends Operator
+ * @property {Operand} a
+ * @property {Operand} b
+ * @property {Operand} c
+ * @param {number} type
+ * @param {Operand} a
+ * @param {Operand} b
+ * @param {Operand} c
+ */
+function TernaryOperator(type, a, b, c) {
+    this.type = type;
+    this.a = a;
+    this.b = b;
+    this.c = c;
+}
+
+/**
+ * @constructor
  * @extends Statement
  * @property {string} text
  * @param {string} [text='']
@@ -84,10 +160,22 @@ util.inherits(TextStatement, Statement);
 /**
  * @constructor
  * @extents Statement
+ * @property {Operand} value
+ * @param {Operand} [value=null]
  */
-function ExpressionStatement() {
+function ExpressionStatement(value) {
+    this.value = typeof value !== 'undefined' ? value : null;
 }
 util.inherits(ExpressionStatement, Statement);
+
+/**
+ * @param {Operand} value
+ * @returns {ExpressionStatement}
+ */
+ExpressionStatement.prototype.setValue = function (value) {
+    this.value = value;
+    return this;
+};
 
 module.exports = {
     /**
@@ -107,10 +195,80 @@ module.exports = {
     },
 
     /**
+     * @param {string} name
+     * @returns {Identifier}
+     */
+    createIdentifier: function (name) {
+        return new Identifier(name);
+    },
+
+    /**
+     * @param {string} value
+     * @returns {Value}
+     */
+    createStringLiteral: function (value) {
+        return new Value(eval('return ' + value));
+    },
+
+    /**
+     * @param {string} value
+     * @returns {Value}
+     */
+    createNumberLiteral: function (value) {
+        return new Value(parseFloat(value));
+    },
+
+    /**
+     * @param {string} value
+     * @returns {Value}
+     */
+    createBooleanLiteral: function (value) {
+        return new Value(value.toLowerCase() === 'true');
+    },
+
+    /**
+     * @returns {Value}
+     */
+    createNullLiteral: function () {
+        return new Value(null);
+    },
+
+    /**
+     * @param {number} type
+     * @param {Operand} a
+     * @returns {UnaryOperator}
+     */
+    createUnaryOperator: function (type, a) {
+        return new UnaryOperator(type, a);
+    },
+
+    /**
+     * @param {number} type
+     * @param {Operand} a
+     * @param {Operand} b
+     * @returns {BinaryOperator}
+     */
+    createBinaryOperator: function (type, a, b) {
+        return new BinaryOperator(type, a, b);
+    },
+
+    /**
+     * @param {number} type
+     * @param {Operand} a
+     * @param {Operand} b
+     * @param {Operand} c
+     * @returns {TernaryOperator}
+     */
+    createTernaryOperator: function (type, a, b, c) {
+        return new TernaryOperator(type, a, b, c);
+    },
+
+    /**
+     * @param {Operand} [value=null]
      * @returns {ExpressionStatement}
      */
-    createExpression: function () {
-        return new ExpressionStatement();
+    createExpression: function (value) {
+        return new ExpressionStatement(value);
     },
 
     /**
