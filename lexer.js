61d1504553e1d9740aa47d6af7205f28ced4383b
Stanislav Yashenkov <vampire@realtymag.ru>
2015-05-03 05:07:09 +0300

    Lexer: OP_LP yield in DEFAULT_MODE

--- lexer.js
+++ lexer.js
@@ -66,7 +66,7 @@ Lexer.WrongIndentError = function (text, pos, line, expectedIndent, actualIndent
 util.inherits(Lexer.WrongIndentError, Lexer.LexerError);
 Lexer.LexerError.prototype.name = 'WrongIndentError';
 
-var defaultModeRE = /([ \t]*\r?\n|\r)|([ \t]+)|([a-z0-9]+(?:[:_-]*[a-z0-9]+)*)|(\.)|(#)|(@)|(\|)|(=)|(,)/gi;
+var defaultModeRE = /([ \t]*\r?\n|\r)|([ \t]+)|([a-z0-9]+(?:[:_-]*[a-z0-9]+)*)|(\.)|(#)|(@)|(\|)|(=)|(,)|(\()/gi;
 var defaultModeTokenTypes = [
     tokens.LF,
     tokens.WHITESPACE,
@@ -76,7 +76,8 @@ var defaultModeTokenTypes = [
     tokens.ATTR_START,
     tokens.TEXT_START,
     tokens.EXPR_START,
-    tokens.OP_COMMA
+    tokens.OP_COMMA,
+    tokens.OP_LP
 ];
 
 var expectTextModeRE = /(\|)|([^\r\n]*)/gi;
